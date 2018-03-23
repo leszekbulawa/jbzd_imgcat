bb005dbfc6ff0c4736bf9d45c326fe434c0d3a2a
Leszek Buława <leszekb@ogre.local>
2018-03-24 00:10:53 +0100

    general refactor; not using imgcat anymore

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # JBZD imgcat
 This small script prints random image from jbzd.pl main page to terminal.
 
-It uses iTerm2 utilities (imgcat) to display images in console.
-
-In future it will cover more terminal emulators and image printing programs.
-Over time it will be available in PyPI.
+Currently it only supports iTerm2.  
+In future it will cover more terminal emulators.  
+Over time it will be available via PyPI.
 
 ## Prequisities
-
 - iTerm2
-- iTerm2 utilities (Shell Integration) - imgcat
 - python 3
 - python3 libs: `requests`, `BeautifulSoup4`
 
 ## Usage
-curl -s "$(python3 get_url.py)" | imgcat
+python3 jbzd_imgcat.py
---
+++ jbzd_imgcat.py
@@ -0,0 +1,28 @@
+import random
+import requests
+from bs4 import BeautifulSoup
+from base64 import b64encode
+from sys import stdout
+
+
+def get_jbzd_random_url():
+    r = requests.get('https://jbzd.pl')
+    soup = BeautifulSoup(r.text, 'html.parser')
+    urls = soup.findAll("img", class_="resource-image")
+    draw_url = urls[random.randint(0, len(urls) - 1)].attrs['src']
+    return draw_url
+
+
+def get_image_content(url):
+    return requests.get(url).content
+
+
+def print_image(data):
+    image_b64_data = b64encode(data)
+    image_size = str(len(image_b64_data))
+    header = bytes('\033]1337;File=;size={};inline=1:'.format(image_size).encode('utf-8'))
+    stdout.buffer.write(header + image_b64_data + b'\a\n')
+
+
+if __name__ == '__main__':
+    print_image(get_image_content(get_jbzd_random_url()))
